0ef874ccb00951a7c2bf2c4502bb75af63dbe937
nguyenquochuy753 <92136604+nguyenquochuy753@users.noreply.github.com>
2022-11-09 09:44:17 +0700

    take refresh token from user , logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const User = require('../models/User');
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
 
+let refreshTokens = [];
 const authController = {
     //Register
     registerUser: async(req,res)=>{
@@ -51,7 +52,7 @@ const authController = {
         try {
             const user = await User.findOne({userName:req.body.userName});
             if(!user)
-                res.status(400).json("Sai tên người người!");
+                res.status(400).json("Sai tên người dùng!");
             const validPassword = await bcrypt.compare(
                 req.body.password,
                 user.password
@@ -62,12 +63,52 @@ const authController = {
             {
                 const accessToken = authController.genarateAccessToken(user);
                 const refreshToken = authController.genarateRefreshToken(user);
+                refreshTokens.push(refreshToken);
+                res.cookie("refreshToken" , refreshToken , {
+                    httpOnly:true,
+                    secure:false,
+                    path:"/",
+                    sameSite: "strict",
+                })
                 const {password, ...others} = user._doc;
-                res.status(200).json({...others,accessToken,refreshToken});
+                res.status(200).json({...others,accessToken});
             }
         } catch (error) {
             res.status(500).json(error)
         }
+    },
+    requestRefreshToken : async(req,res)=>{
+        // Lấy refreshToken từ người dùng
+        const refreshToken = req.cookies.refreshToken;
+        //Kiểm tra có refreshToken không 
+        if(!refreshToken)
+            return res.status(401).json("You're not authenticated");
+        //Kiểm tra xem refreshToken có trong refreshTokens của mình hay không?
+        if(!refreshTokens.includes(refreshToken))
+            return res.status(403).json("Refresh token is not valid");
+        //Xác nhận refreshToken đúng không
+        jwt.verify(refreshToken , process.env.JWT_REFRESH_KEY , (err,user)=>{
+            if(err)
+                console.log(err);
+            refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+            const newAccessToken = authController.genarateAccessToken(user);
+            const newRefreshToken = authController.genarateRefreshToken(user);
+            refreshTokens.push(newRefreshToken);
+            res.cookie("refreshToken", newRefreshToken, {
+                httpOnly: true,
+                secure: false,
+                path: "/",
+                sameSite: "strict",
+            });
+            res.status(200).json({accessToken : newAccessToken});
+        })
+    },
+    Logout : async(req , res) =>{
+        res.clearCookie("refreshToken");
+        refreshTokens = refreshTokens.filter(
+            (token) => token !== req.cookies.refreshToken 
+        );
+        res.status(200).json("Logged out !");
     }
 }
 
