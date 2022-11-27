4619ee5084633d920ee2b0ad78fce311442ccca1
Numb246 <113155975+hvuquang@users.noreply.github.com>
2022-11-27 21:49:32 +0700

    deletePost API

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,35 +1,49 @@
-const postModel = require('../models/Post');
+const postModel = require("../models/Post");
 
 const postController = {
-    addPost : async(req , res)=>{
-        const post = new postModel({
-            title: req.body.title,
-            des: req.body.des,
-        })
-        if(req.files){
-            let path = '';
-            req.files.forEach((function (files, index, arr) {
-                path = path + files.path + ','
-            }));
-            path=path.substring(0 , path.lastIndexOf(','));
-            post.imgURLs = path;
-        }
-        try {
-            await post.save();
-            res.send(post);
-        } catch (error) {
-            res.status(500).json(error);
-        }
-    },
-    readPost: async(req,res) => {
-        try{
-            const posts= await postModel.find({});
-            res.status(200).json(posts);
-        } catch (error) {
-            res.status(500).json(error);
-        }
+  addPost: async (req, res) => {
+    const post = new postModel({
+      title: req.body.title,
+      des: req.body.des,
+    });
+    if (req.files) {
+      let path = "";
+      req.files.forEach(function (files, index, arr) {
+        path = path + files.path + ",";
+      });
+      path = path.substring(0, path.lastIndexOf(","));
+      post.imgURLs = path;
     }
-
-}
+    try {
+      await post.save();
+      res.send(post);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+  readPost: async (req, res) => {
+    try {
+      const posts = await postModel.find({});
+      res.status(200).json(posts);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+  deletePost: async (req, res) => {
+    const postID = req.body.postID;
+    postModel
+      .findByIdAndDelete(postID)
+      .then(() => {
+        res.json({
+          message: "Post deleted successfully",
+        });
+      })
+      .catch((error) => {
+        res.json({
+          message: "Failed to delete post",
+        });
+      });
+  },
+};
 
 module.exports = postController;
--- routes/post.js
+++ routes/post.js
@@ -4,4 +4,5 @@ const upload = require('../middleware/upload');
 
 router.post('/addPost', upload.array('imgURLs') , postController.addPost);
 router.get('/readPost', postController.readPost);
+router.delete('/deletePost', postController.deletePost);
 module.exports = router;
